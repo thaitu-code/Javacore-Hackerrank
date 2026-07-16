a9f8d7b05b1c61a63f3b541ec74311daeaf30390
Thai Van Tu <thaitu19005678@gmail.com>
2026-07-16 09:59:56 +0700

    JavaIfElse

---
+++ JavaIfElse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class JavaIfElse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n % 2 == 1){
+            System.out.println("Weird");
+        }
+        else{
+            if(n >= 2 && n <= 5){
+                System.out.println("Not Weird");
+            }
+            else if(n <= 20){
+                System.out.println("Weird");
+            }
+            else System.out.println("Not Weird");
+        }
+        sc.close();
+    }
+}
